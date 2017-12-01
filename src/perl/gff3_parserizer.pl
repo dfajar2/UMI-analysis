55c1e606b87db61973a0689762a8613bd98838ea
Callum Bell <cjb@ncgr.org>
2017-11-30 17:16:52 -0700

    Added some comments. Improved parsing of GFF attributes field to pull out just the gene id

--- src/perl/gff3_parserizer.pl
+++ src/perl/gff3_parserizer.pl
@@ -2,23 +2,40 @@
 
 use strict;
 
+# DO NOT USE FOR TRANSCRIPTOME ALIGNMENTS.
+#  
 # Rip a GFF3 file into a structure suitable for fast look ups
-# of genes just based on a position.
+# of genes just based on a position. A position is a chromosome
+# or transcript and a coordinate:
+#
+# Chr01:11050
+#
+# Coordinate is made out of 1st, 4th and 5th columns of the GFF file.
+# There is a position made for every coordinate in a gene. This is a hash key.
+# The hash value is the last column in the GFF, including the name of the gene. 
+# So you can look up any position and find out what gene it is in. Like:
+#
+# Chr01:63062 => ID=Pp3c1_100;gene_id=Pp3c1_100;ancestorIdentifier=Pp3c1_100.v3.1
+#
 
-
-
-unless ($ARGV[0])
+unless ($ARGV[1])
 {
-   print STDERR "\nUsage: $0 gff_file umi_file\n\n";
+   print STDERR "\nUsage: $0 gff_file positions_umi_counts\n\n";
    exit 1;
 }
 
-
+#
+# Specify GFF3 file and a file of positions and UMI counts.
+#
 my ($gff_file,$umi_file) = @ARGV;
 
 my $lookup_dataref;
 my $output_dataref;
 my $i = 0;
+
+#
+# You can look up different features. The default is gene
+#
 my $target_feature = "gene";
 
 print STDERR "Counting GFF lines... \n";
@@ -48,7 +65,11 @@ while (<GFF>)
    {
       next;
    }
-      
+   
+   #
+   # This builds the big hash for looking up gene belonging to a position, like:
+   # Chr01:63062 => ID=Pp3c1_100;gene_id=Pp3c1_100;ancestorIdentifier=Pp3c1_100.v3.1
+   #   
    for my $nuc ($start ..$stop )
    {
       my $position = $chr . ":" . $nuc;
@@ -65,7 +86,16 @@ print STDERR "Finished making the structure. Check memory usage... \n";
 #    ;
 # }
 
-# Open clusterized_umi_counts[2].txt to look up the lookup_dataref
+# Clusterized umi counts
+# Chr01:10482857  25
+# Chr01:10482858  70
+# Chr01:10482859  90
+# Chr01:10482860  33
+# Chr01:10482861  11
+# Chr01:10482862  5
+#
+# FOR genome alignments you have to do the lookup.
+#
 
 open (UMI, "<$umi_file")|| die "Couldn't open the file $!";
 
@@ -77,6 +107,7 @@ my $linecount = `wc -l $umi_file`;
 ($linecount) = $linecount =~ /(\d+)/;
 
 print STDERR "Doing lookups... \n";
+
 while (<UMI>)
 {  
    $i++;
@@ -87,11 +118,16 @@ while (<UMI>)
    }
    
    chomp;
+   
    my ($position,$umi_count) = split /\s+/;
    
    if (my $attributes = $lookup_dataref->{$position})
    {
-      $output_dataref->{$attributes}+=$umi_count;
+      #
+      # This may need to be changed depending on the format of the last field in the GFF file
+      #
+      my ($gene) = $attributes =~ /gene_id=(.*?);/;
+      $output_dataref->{$gene}+=$umi_count;
    }
 }
  
